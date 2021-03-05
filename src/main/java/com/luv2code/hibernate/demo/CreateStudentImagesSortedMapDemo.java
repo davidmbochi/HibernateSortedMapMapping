67a4bd9e652af0f710a2b8ba0707d8b6485389ba
davidmbochi <davidmbochi97@gmail.com>
2021-03-05 14:35:36 +0300

    initial commit

---
+++ src/main/java/com/luv2code/hibernate/demo/CreateStudentImagesSortedMapDemo.java
@@ -0,0 +1,46 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+
+
+public class CreateStudentImagesSortedMapDemo {
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        //create session
+        Session session = factory.getCurrentSession();
+        try {
+            //create the object
+            Student tempStudent = new Student("john","Doe","john@luv2code.com");
+            Map<String,String> theImages = tempStudent.getImages();
+
+            theImages.put("photo1.jpg","photo 1");
+            theImages.put("photo2.jpg","photo 2");
+            theImages.put("photo3.jpg","photo 3");
+
+            //start a transaction
+            session.beginTransaction();
+            //save the object
+            System.out.println("Saving the student and images");
+            session.persist(tempStudent);
+            //commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!");
+        }finally {
+            //clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+}
